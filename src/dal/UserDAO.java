0943127c5f7fb77c68e7b126b1feca1acadbb992
Sebastian T. Bakonyvari <twast.92@gmail.com>
2017-02-16 13:23:36 +0100

    Added missing fields + getters/setters

--- src/dal/UserDAO.java
+++ src/dal/UserDAO.java
@@ -1,7 +1,4 @@
 package dal;
 
-/**
- * Created by twast on 13/02/2017.
- */
 public class UserDAO {
 }
--- src/dto/UserDTO.java
+++ src/dto/UserDTO.java
@@ -9,9 +9,10 @@ public class UserDTO implements Serializable{
 	private static final long serialVersionUID = 4545864587995944260L;
 	private int	userId;                     
 	private String userName;                
-	private String ini;                 
+	private String ini;
+	private int cpr;
+	private String password;
 	private List<String> roles;
-	//TODO Add relevant fields
 	
 	public UserDTO() {
 		this.roles = new ArrayList<>();
@@ -20,25 +21,47 @@ public class UserDTO implements Serializable{
 	public int getUserId() {
 		return userId;
 	}
+
 	public void setUserId(int userId) {
 		this.userId = userId;
 	}
+
 	public String getUserName() {
 		return userName;
 	}
+
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+
 	public String getIni() {
 		return ini;
 	}
+
 	public void setIni(String ini) {
 		this.ini = ini;
 	}
 
-	public List<String> getRoles() {
+    public int getCpr() {
+        return cpr;
+    }
+
+    public void setCpr(int cpr) {
+        this.cpr = cpr;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<String> getRoles() {
 		return roles;
 	}
+
 	public void setRoles(List<String> roles) {
 		this.roles = roles;
 	}
@@ -57,7 +80,7 @@ public class UserDTO implements Serializable{
 
 	@Override
 	public String toString() {
-		return "UserDTO [userId=" + userId + ", userName=" + userName + ", ini=" + ini + ", roles=" + roles + "]";
+		return "UserDTO [userId=" + userId + ", userName=" + userName + ", ini=" + ini + ", cpr=" + cpr + ", password=" + password + ", roles=" + roles + "]";
 	}
 	
 	
