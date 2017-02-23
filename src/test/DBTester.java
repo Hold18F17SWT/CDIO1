001977bc195ce260b888677d5d74d3c8d96ec9eb
Frederik <s165249@student.dtu.dk>
2017-02-23 11:48:33 +0100

    Staarted tests

--- src/test/DBTester.java
+++ src/test/DBTester.java
@@ -1,36 +1,84 @@
 package test;
 
-
+//UTIL IMPORT
 import java.util.List;
 
+//CLASSES IMPORT
 import dal.IUserDAO;
 import dal.IUserDAO.DALException;
 import dal.UserDAODiscImpl;
 import dto.UserDTO;
 
-public class DBTester {
-	//TODO refactor as JUnit test???
-	public static void main(String[] args) {
-		IUserDAO iDAO = new UserDAODiscImpl();
-		UserDTO newUser = new UserDTO(1, "","","");
-		printUsers(iDAO);
-		//TODO test new fields...
-		newUser.setIni("test");
-		newUser.addRole("Admin");
-		newUser.setUserName("testName");
-		newUser.setUserId(0);
-		try {
-			iDAO.createUser(newUser);
-		} catch (DALException e) {
-			e.printStackTrace();
-		}
+//JUNIT4 IMPORTS
+import org.junit.Test;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.After;
 
+
+public class DBTester {
+	private UserDTO User1 = new UserDTO(1,"", "", "");
+	private UserDTO User2_Admin = new UserDTO(1,"", "", "");
+	private UserDTO User3 = new UserDTO(1,"", "", "");
+
+	private IUserDAO iDAO = new UserDAODiscImpl();
+
+
+
+	@Before
+	public void setUp() throws Exception {
+		User1.setIni("test");
+		User1.setUserId(0);
+		User1.setUserName("Bjarne");
+		User1.setCpr("12345678-9012");
+
+		User2_Admin.setIni("test");
+		User2_Admin.addRole("Admin");
+		User2_Admin.setUserId(1);
+		User2_Admin.setCpr("12345678-9013");
+		User2_Admin.setUserName("AdminMan");
+
+		User3.setIni("test");
+		User3.setUserId(2);
+		User3.setUserName("Bjarne1");
+		User3.setCpr("12345678-9014");
+
+		iDAO.createUser(User1);
+		iDAO.createUser(User2_Admin);
+		iDAO.createUser(User3);
+	}
+
+	@Test
+	public void testEntities(){
+
+		Assert.assertNotNull(this.User1);
+		Assert.assertNotNull(this.User2_Admin);
+		Assert.assertNotNull(this.User3);
+
+		printUsers(iDAO);
+
+	}
+	@Test(expected = DALException.class)
+	public void testUserAlreadyExsits() {
+		try {
+			iDAO.createUser(User1);
+		} catch (DALException e1){
+		Assert.assertTrue(e1.getMessage().equals("User already exists!"));
+		}
+	}
+
+/*
+	public static void main(String[] args) {
+		//TODO test new fields...
+
+		//throw error if user already exsists
 		try {
 			iDAO.createUser(newUser);
 		} catch (DALException e1) {
 			System.out.println("User already existed - OK");
 		}
-	
+
+		//create new user
 		newUser.setUserId(1);
 		newUser.setUserName("2ND user");
 		try {
@@ -57,10 +105,10 @@ public class DBTester {
 		}
 		
 		printUsers(iDAO);
-		
-		
-	}
 
+	}
+*/
+	//Change to new arrayList
 	private static void printUsers(IUserDAO iDAO) {
 		try {
 			System.out.println("Printing users...");
@@ -72,6 +120,11 @@ public class DBTester {
 		} catch (DALException e) {
 			e.printStackTrace();
 		}
+	}
+
+	@After
+public void tearDown() throws Exception{
+
 	}
 
 }
